60c5cc3e4d84a9a98e71a1c8338b020461c4754a
sebiddy <sebiddy@gmail.com>
2014-07-30 09:32:59 +0100

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,191 @@
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+var yelp = require('yelp');
+var keys = require('../keys.js')
+
+var globalRes = '';
+var globalReq = '';
+
+//The url's (incl. parameters) for the api's
+var apiUrls = {
+	weather: function(latLng) {
+		var url =
+			'http://api.worldweatheronline.com/free/v1/weather.ashx?key=' +
+			keys.weather + 
+			'&format=json&q=' +
+			latLng.lat +
+			',' +
+			latLng.lng;
+		return url;
+	},
+	google: {
+		distance: function(origin, destination, mode) {
+			var url = 
+				'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' +
+				origin.lat +
+				',' +
+				origin.lng +
+				'&destinations=' +
+				destination.lat +
+				',' +
+				destination.lng +
+				'&mode=' +
+				mode +
+				'&key=' +
+				keys.google;
+			return url;
+		}
+	}
+};
+
+//Boilerplate code for getting JSON response
+function httpGet(url, callback) {
+	http.get(url, function(res) {
+		var chunks = [];
+		res.on('data', function(chunk) {
+			chunks.push(chunk);
+		});
+		res.on('end', function() {
+			var body = chunks.join('');
+			//If it is a string, parse to JSON
+			if(typeof body === 'string') {
+				body = JSON.parse(body);
+			}
+			//Pass JSON response to callback
+			callback(body);
+		});
+  });
+}
+
+/* GET home page. */
+router.get('/', function(req, res) {
+	globalReq = req;
+	globalRes = res;
+
+	//Get weather from latitude/longitude in request
+	httpGet(apiUrls.weather({
+		lat: globalReq.query.lat,
+		lng: globalReq.query.lng
+	}), getWeather());
+});
+
+function getWeather(weather) {
+	//These are the only weather codes which are 'good'
+	//(i.e. not rain)
+	var goodWeatherCodes = [113, 116, 119, 122, 143];
+	//All weather Yelp categories to search for
+	var yelpCategories = 'aquariums,leisure_centers,galleries,museums,';
+	//Weather code from location (`+` converts to number)
+	var weatherCode = +weather.data.current_condition[0].weatherCode;
+	//If `weatherCode` is not is in `goodWeatherCodes` then add outdoor Yelp categories
+	if(goodWeatherCodes.indexOf(weatherCode) !== -1) {
+		yelpCategories += 'mini_golf,parks,skatingrinks,zoos';
+	}
+	getYelp(yelpCategories);
+}
+
+function getYelp(yelpCategories) {
+	var yelpClient = yelp.createClient(keys.yelp);
+	//Search yelp
+	yelpClient.search({
+		location: 'London',
+		category_filter: yelpCategories
+	}, removeYelpDuplicateJSON);	
+}
+
+function removeYelpDuplicateJSON(error,data) {
+	//Remove duplicate categories
+	//Does not remove categories where there are duplicates...
+	//...but with an additional different category
+	var duplicateCateogryPOIs = data.businesses;
+	var categoryArray = [];
+	var filteredCateogryPOIs = [];
+	duplicateCateogryPOIs.forEach(function(POI) {
+		var alreadyExists = false;
+		categoryArray.forEach(function(category) {
+			//Compare nested arrays by converting to strings
+			if(JSON.stringify(POI.categories) === JSON.stringify(category)) {
+				//If category already exists set to `true`
+				alreadyExists = true;
+			}
+		});
+		//If category does not exist already, and has coordidante field...
+		//...add to non-duplicate POI array
+
+		if(!alreadyExists && POI.location.coordidnate) {
+			categoryArray.push(POI.categories);
+			filteredCateogryPOIs.push(POI);
+		}
+	});
+	getDistancesFromOrigin(filteredCateogryPOIs);
+}
+
+function getDistancesFromOrigin(POIs) {
+	//Loop through all POI's
+	//Keep a total to know when requests are done as they're async.
+	var total = 0;
+	POIs[0].distance = 0;
+	var originPOI = POIs[0];
+	var finalPOIs = [originPOI];
+
+	function getDistance(POI) {
+		httpGet(apiUrls.google.distance({
+				origin: {
+					lat: originPOI.location.coordinate.latitude,
+					lng: originPOI.location.coordinate.longitude
+				},
+				destination: {
+					lat: POI.location.coordinate.latitude,
+					lng: POI.location.coordinate.longitude
+				},
+				mode: 'walking'
+		}), function(json) {
+			var POIDistanceIncluded = POI.distance = json.rows[0].elements[0].distance.value;
+			finalPOIs.push(POIDistanceIncluded);
+			if(++total === 5) {
+				makeLoop(finalPOIs);
+			}
+		});
+	}
+
+	for(var i = 1; i < 5; ++i) {
+		getDistance(POIs[i]);
+	}
+}
+
+//Sort POI's by distance from origin (0)
+//Then form them into a 'loop'
+function makeLoop(POIs) {
+	//Sorts by distance in increasing order
+	//(i.e. 3,4,1,2 -> 1,2,3,4)
+	function sortByDistance(a, b) {
+		if(a.distance < b.distance) {
+			return -1;
+		} else if(a.distance > b.distance) {
+			return 1;
+		}
+		return 0;
+	}
+
+	//Creates 'loop' from sorted values
+	//(i.e. 1,2,3,4 -> 14321)
+	function formLoop(arr) {
+		var b = [arr[arr.length-1]];
+		for(var i = arr.length-2; i >= 0; --i) {
+			if(!(i % 2)) {
+				b.unshift(arr[i]);
+			} else {
+				b.push(arr[i]);
+			}
+		}
+		b.push(arr[0]);
+		return b;
+	}
+
+	var sortedPOIs = POIs.sort(sortByDistance);
+	var loopedPOIs = formLoop(sortedPOIs);
+	globalRes.json(loopedPOIs);
+}
+
+module.exports = router;
