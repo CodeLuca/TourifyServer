6575194a25691345e53a484f3be5c7116903b1c2
sebiddy <sebiddy@gmail.com>
2014-08-02 01:19:53 +0100

    Add map

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	var json = '';
+
 	/*Event handlers*/
 
 	$('#startTour button').click(getCoordinates);
@@ -10,23 +12,41 @@ $(document).ready(function() {
 	/*Event handler functions*/
 
 	function getMap() {
-		var directionsService = new google.maps.DirectionsService();
-		var directionsDisplay;
 		var source = $('#mapTemplate').html();
 		var template = Handlebars.compile(source);
 		$('#wrapper').fadeOut(function() {
 			$('#wrapper').html(template());
 			$('#wrapper').fadeIn();
 		});
-		$('body').on('ready', '#mapCanvas', function() {
-			alert('Ready')
-			var mapOptions = {
-          		center: new google.maps.LatLng(-34.397, 150.644),
-          		zoom: 8
-        	};
-        	var map = new google.maps.Map(document.getElementById("mapCanvas"),mapOptions);
-      	})
-      	alert('Hi')
+		setTimeout(function() {
+			var directionsService = new google.maps.DirectionsService();
+			var directionsDisplay = new google.maps.DirectionsRenderer();
+
+			var waypointsArray = [];
+			for(var i = 1; i < json.results.length; ++i) {
+				waypointsArray.push({
+					location: new google.maps.LatLng(json.results[i].geometry.location.lat, json.results[i].geometry.location.lng)
+				})
+			}
+			directionsService.route({
+				origin: new google.maps.LatLng(json.results[0].geometry.location.lat, json.results[0].geometry.location.lng),
+				destination: new google.maps.LatLng(json.results[0].geometry.location.lat, json.results[0].geometry.location.lng),
+				travelMode: google.maps.TravelMode.WALKING,
+				waypoints: waypointsArray
+			}, function(result, status) {
+				if(status === 'OK') {
+					var mapOptions = {
+	        			center: new google.maps.LatLng(json.results[0].geometry.location.lat, json.results[0].geometry.location.lng),
+	        			zoom: 18
+	       			};
+	        		var map = new google.maps.Map(document.getElementById("mapCanvas"),mapOptions);
+	        		directionsDisplay.setMap(map);
+	        		directionsDisplay.setDirections(result)
+				} else {
+					alert(JSON.stringify(status))
+				}
+			});
+   		}, 1000);
    	}
 
 	function showAjaxSpinner() {
@@ -70,8 +90,8 @@ $(document).ready(function() {
 			if(status === 'OK') {
 				getLocationParams({
 					coords: {
-					latitude: result[0].geometry.location.lat(),
-					longitude: result[0].geometry.location.lng()
+						latitude: result[0].geometry.location.lat(),
+						longitude: result[0].geometry.location.lng()
 					}
 				});
 			} else {
@@ -106,6 +126,7 @@ $(document).ready(function() {
 
 	function getJson(paramObj) {
 		$.get('/json', paramObj, function(data) {
+			json = data;
 			data.numberOfResults = data.numberOfResults-1;
 			nextPage(data);
 		})
